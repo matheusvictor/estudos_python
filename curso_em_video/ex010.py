43a1aa27477cb3c19c7e4bbb7d1c7105fa054310
Matheus Victor <matheusvictor.salles@gmail.com>
2021-07-20 10:18:28 -0300

    corrige ex10

--- curso_em_video/ex010.py
+++ curso_em_video/ex010.py
@@ -1,3 +1,3 @@
 reais = float(input('Digite quantos reais você possui na carteira: R$'))
-dolar = reais * 3.27 #valor considerado do dólar = R$3,27
-print('Você possui o equivalente à U${}.'.format(dolar))
+dolar = reais / 5.28
+print(f'Com R${reais:.2f}, você pode comprar U${dolar:.2f}.')
