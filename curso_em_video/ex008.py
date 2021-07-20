12bdb7293929c1e9dd202dc602f8f0a43d06c798
Matheus Victor <matheusvictor.salles@gmail.com>
2021-07-20 10:07:15 -0300

    modifica ex008

--- curso_em_video/ex008.py
+++ curso_em_video/ex008.py
@@ -1,4 +1,11 @@
-numero = float(input('Digite um valor em metros para obter sua conversão em cm e mm: '))
+numero = float(input('Digite um valor em metros para obter sua conversão em km, hm, dam, dm, cm e mm: '))
+
+km = numero/1000
+hm = numero/100
+dam = numero/10
+dm = numero*10
 cm = numero*100
 mm = numero*1000
-print('{}m equivale a {:.1f}cm ou {:.1f}mm!'.format(numero, cm, mm))
+
+print(f'{numero}m equivale às conversões abaixo:')
+print(f'{km:.3f}km\n{hm:.2f}hm\n{dam:.1f}dam\n{dm:.1f}dm\n{cm:.1f}cm\n{mm:.1f}mm')
