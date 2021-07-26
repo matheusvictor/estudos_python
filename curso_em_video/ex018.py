512a49d3146badefc0a85b1b3fa1b3d245c59506
Matheus Victor <matheusvictor.salles@gmail.com>
2021-07-26 16:54:27 -0300

    resolve ex018

---
+++ curso_em_video/ex018.py
@@ -0,0 +1,3 @@
+from math import sin, cos, tan, degrees
+angulo = float(input('Digite um valor de ângulo qualquer: '))
+print(f'O ângulo de {angulo}º tem uma tangente de {tan(angulo)} graus radianos, cosseno de {cos(angulo)} e seno de {sin(angulo)}')
