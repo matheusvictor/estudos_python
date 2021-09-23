b4a65250805cba263f228759d6146505ee84e305
Matheus Victor <matheusvictor.salles@gmail.com>
2021-09-23 19:32:56 -0300

    Adiciona exercício 24 do Curso em Vídeo

---
+++ curso_em_video/ex024.py
@@ -0,0 +1,5 @@
+nome_cidade = input("Digite o nome de uma cidade: ")
+nome_cidade_dividido = nome_cidade.split()
+primeiro_nome_cidade = nome_cidade_dividido[0]
+
+print("SANTO" in primeiro_nome_cidade)
