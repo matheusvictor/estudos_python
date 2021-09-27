e2b36d40f54d2f8a95cac147596a142188482255
Matheus Victor <matheusvictor.salles@gmail.com>
2021-09-27 11:25:03 -0300

    Otimizando importação de biblioteca

---
+++ curso_em_video/ex028.py
@@ -0,0 +1,8 @@
+from random import randomint
+numero_gerado = random.randint(0, 5)
+numero_digitado = int(input("Digite um número: "))
+print(f'O número gerado foi {numero_gerado}. Você digitou o número {numero_digitado}.')
+if numero_digitado == numero_gerado:
+    print("Você acertou!")
+else:
+    print("Você errou!")
