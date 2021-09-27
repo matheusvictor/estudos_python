4059bcf38ff4d36db15c720f5adb41bf4d7bd9be
Matheus Victor <matheusvictor.salles@gmail.com>
2021-09-27 11:52:14 -0300

    Incrementando a questão 32

--- curso_em_video/ex032.py
+++ curso_em_video/ex032.py
@@ -1,5 +1,9 @@
-ano = int(input("Digit o ano para saber se ele é bissexto: "))
+from datetime import date # importando biblioteca de datas
 
+ano = int(input("Digite o ano a ser analisado e para saber se ele é bissexto. Caso queira analisar o ano atual, digite 0: "))
+
+if(ano == 0):
+    ano = date.today().year # o ano vai ser igual ao ano do dia atual
 '''
 Chama-se ano bissexto o ano ao qual é acrescentado um dia extra,
 ficando com 366 dias, um dia a mais do que os anos normais de 365 dias, ocorrendo a cada quatro anos
@@ -9,4 +13,4 @@ ficando com 366 dias, um dia a mais do que os anos normais de 365 dias, ocorrend
 if(ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0):
     print(f'O ano {ano} é BISSEXTO!')
 else:
-    print(f'O ano {ano} é não é NÃO É BISSEXTO!')
+    print(f'O ano {ano} NÃO É BISSEXTO!')
