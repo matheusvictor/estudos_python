91f65778a16eadbdcb681cce9b1e74b50d72a9ab
Matheus Victor <matheusvictor.salles@gmail.com>
2021-09-28 10:16:06 -0300

    Otimizando questão 33

--- curso_em_video/ex033.py
+++ curso_em_video/ex033.py
@@ -5,19 +5,16 @@ terceiro_numero = input("Digite o 3º número: ")
 maior_numero = primeiro_numero
 menor_numero = primeiro_numero
 
-if(segundo_numero > primeiro_numero and segundo_numero > terceiro_numero and primeiro_numero > terceiro_numero):
+#verificando maior número:
+if(segundo_numero > primeiro_numero and segundo_numero > terceiro_numero):
     maior_numero = segundo_numero
-    menor_numero = terceiro_numero
-elif(segundo_numero > primeiro_numero and segundo_numero > terceiro_numero and segundo_numero > primeiro_numero):
-    maior_numero = segundo_numero
-elif(terceiro_numero > primeiro_numero and terceiro_numero > segundo_numero and primeiro_numero > segundo_numero):
+elif(terceiro_numero > primeiro_numero and terceiro_numero > segundo_numero):
     maior_numero = terceiro_numero
+
+#verificando menor número:
+if(segundo_numero < primeiro_numero and segundo_numero < terceiro_numero):
     menor_numero = segundo_numero
-elif(terceiro_numero > primeiro_numero and terceiro_numero > segundo_numero and segundo_numero > primeiro_numero):
-    maior_numero = terceiro_numero
-elif(primeiro_numero > segundo_numero and primeiro_numero > terceiro_numero and segundo_numero > terceiro_numero):
+elif(terceiro_numero < primeiro_numero and terceiro_numero < segundo_numero):
     menor_numero = terceiro_numero
-elif (primeiro_numero > segundo_numero and primeiro_numero > terceiro_numero and terceiro_numero > segundo_numero):
-    menor_numero = segundo_numero
 
 print(f'{maior_numero} é o maior número digitado e {menor_numero} é o menor.')
