0820c823ab068b35d500b7e6bfb0cb5622b22969
Matheus Victor <matheusvictor.salles@gmail.com>
2021-09-28 14:03:29 -0300

    Adiciona cores ao exercício

--- curso_em_video/ex035.py
+++ curso_em_video/ex035.py
@@ -3,6 +3,6 @@ segunda_reta = int(input("Digite o comprimento da 2ª reta: "))
 terceira_reta = int(input("Digite o comprimento da 3ª reta: "))
 
 if(primeira_reta < segunda_reta + terceira_reta and segunda_reta < primeira_reta + terceira_reta and terceira_reta < primeira_reta + segunda_reta):
-    print("Os segmentos acima podem formam um triângulo!")
+    print("\033[1;30;42mOs segmentos acima podem formam um triângulo!\033[m")
 else:
-    print("Os segmentos acima não podem formam um triângulo!")
+    print("\033[1;7;30;41mOs segmentos acima não podem formam um triângulo!\033[m")
