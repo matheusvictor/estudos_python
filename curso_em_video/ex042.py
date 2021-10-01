02e3ff33269a7c71215ca6e5bcca8cec1e602eb7
Matheus Victor <matheusvictor.salles@gmail.com>
2021-10-01 11:10:34 -0300

    Modifica resolução da questão 42 do Curso em Vídeo

--- curso_em_video/ex042.py
+++ curso_em_video/ex042.py
@@ -5,12 +5,12 @@ terceira_reta = int(input("Digite o comprimento da 3ª reta: "))
 if(primeira_reta < segunda_reta + terceira_reta and segunda_reta < primeira_reta + terceira_reta and terceira_reta < primeira_reta + segunda_reta):
     print("\033[1;30;42mOs segmentos acima podem formam um triângulo!\033[m")
 
-    if(primeira_reta == segunda_reta and primeira_reta == terceira_reta and segunda_reta == terceira_reta):
+    if(primeira_reta == segunda_reta == terceira_reta):
         print("Como todos os lados são iguais... formam um triângulo EQUILÁTERO!")
-    elif(primeira_reta == segunda_reta or primeira_reta == terceira_reta or segunda_reta == terceira_reta):
-        print("Como há pelo menos dois lados iguais... formam um triângulo ISÓSCELES!")
-    else:
+    elif(primeira_reta != segunda_reta != terceira_reta != primeira_reta):
         print("Como todos os lados são diferentes... formam um triângulo ESCALENO!")
+    else:
+        print("Como há pelo menos dois lados iguais... formam um triângulo ISÓSCELES!")
 
 else:
     print("\033[1;7;30;41mOs segmentos acima não podem formam um triângulo!\033[m")
