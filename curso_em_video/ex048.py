459d442eda42f96a659aad5edc9fcb33510700f3
Matheus Victor <matheusvictor.salles@gmail.com>
2021-10-06 10:38:52 -0300

    Otimiza resolução da questão 48 do Curso em Vídeo

--- curso_em_video/ex048.py
+++ curso_em_video/ex048.py
@@ -1,5 +1,5 @@
 soma = 0
-for numero in range(1,501):
+for numero in range(1,501,2):
     if(numero % 3 == 0):
         soma += numero
 print(f'A soma dos números ímpares e múltiplos de três entre 1 e 500 é igual a: {soma}')
