815feb5feb787a8b988582c8261c5620bf63d34d
Matheus Victor <matheusvictor.salles@gmail.com>
2021-10-06 10:27:41 -0300

    Add resolução da questão 46 do Curso em Vídeo

---
+++ curso_em_video/ex046.py
@@ -0,0 +1,7 @@
+from time import sleep
+import emoji
+print('ATENÇÃO PARA CONTAGEM REGRESSIVA PARA O SHOW DE FOGOS DE ARTIFÍCIOS!')
+for contador in range(10, 0, -1):
+    print(contador)
+    #sleep(1)
+print(emoji.emojize(':party_popper: :confetti_ball: :tada: :confetti_ball: :fireworks:', use_aliases=True))
