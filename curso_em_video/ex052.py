8b6ef141dbf555793b232cbc25b0c2d9f8562c4a
Matheus Victor <matheusvictor.salles@gmail.com>
2021-10-07 11:09:09 -0300

    Corrige questão ex052.py

--- curso_em_video/ex052.py
+++ curso_em_video/ex052.py
@@ -1,11 +1,15 @@
 numero = int(input('Digite um número inteiro para saber se ele é primo: '))
-
 numero_divisores = 0
+
 for contador in range(1, numero+1):
     if (numero % contador == 0):
-        numero_divisores +=1
+        print('\033[33m', end='')
+        numero_divisores += 1
+    print(f'{contador}\033[m', end=' ')
 
-if(numero_divisores > 2):
-    print(f'O número {numero} não é primo!')
+print(f'\nO número {numero} foi dividido {numero_divisores} vez(es).')
+
+if(numero_divisores == 2):
+    print('Por isso, ele \033[34mé\033[m um número primo!')
 else:
-    print(f'O número {numero} é primo!')
+    print('Por isso, ele \033[31mnão é\033[m um número primo!')
