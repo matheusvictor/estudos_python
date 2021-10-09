16a8ddceb0c83143a429a3f8804c245851083542
Matheus Victor <matheusvictor.salles@gmail.com>
2021-10-09 10:24:43 -0300

    Modifica resolução da questão ex060.py

--- curso_em_video/ex060.py
+++ curso_em_video/ex060.py
@@ -1,10 +1,11 @@
-numero = float(input('Digite um número qualquer para saber seu fatorial: '))
+numero = int(input('Digite um número qualquer para saber seu fatorial: '))
 contador = numero
-fatorial = numero
+fatorial = 1 #inicia variável com fator nulo da multiplicação
 
-while(contador > 1):
+while(contador > 0):
+    print(contador, end='')
+    print(' x ' if contador > 1 else ' = ', end='')
+    fatorial *= (contador) #na 1ª vez do lado, fatorial passará a ser o numero digitado, pois 1 * n = n
     contador -= 1
-    fatorial *= (contador)
-    print(contador)
 
-print(fatorial)
+print(f'{numero}! = {fatorial}')
