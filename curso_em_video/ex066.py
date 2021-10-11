8a9a8cfcb189fde0b02ae5ef2d5e6ef50afa9fa6
Matheus Victor <matheusvictor.salles@gmail.com>
2021-10-11 10:11:44 -0300

    Modifica resolução de ex066.py

--- curso_em_video/ex066.py
+++ curso_em_video/ex066.py
@@ -3,11 +3,11 @@ numero = soma = contador = 0
 while(numero != 999):
     numero = int(input('Digite um número (insira 999 se quiser parar o programa): '))
 
-    if(numero != 999):
-        soma += numero
-        contador += 1
-    else:
+    if(numero == 999):
         break
-
+    soma += numero
+    contador += 1
+    
+print('-=' * 30)
 print(f'Você digitou {contador} números e a soma entre eles é igual a {soma}!')
 print('Programa encerrado!')
