93555f0f361cde356a39a4be1262b8458c2894c6
Matheus Victor <matheusvictor.salles@gmail.com>
2022-02-03 08:22:52 -0300

    Modifica resolução do ex103

--- curso_em_video/mundo_03/ex103.py
+++ curso_em_video/mundo_03/ex103.py
@@ -1,12 +1,15 @@
-def ficha(nome='', gols=''):
-    if nome == '':
-        nome = '<desconhecido>'
-    if gols == '':
-        gols = int('0')
-
-    print(f'O jogador {nome.capitalize()} fez {gols} gols no campeonato.')
+def ficha(nome='<desconhecido>', gols=0):
+    print(f'O jogador {nome.capitalize()} fez {gols} gol(s) no campeonato.')
 
 
 nome_jogador = str(input('Nome do jogador: '))
-qtd_gols = input('Gols: ')
-ficha(nome_jogador, qtd_gols)
+qtd_gols = input('Número de gols: ')
+
+if qtd_gols.isnumeric():
+    qtd_gols = int(qtd_gols)
+else:
+    qtd_gols = 0
+if nome_jogador.strip() == '':
+    ficha(gols=qtd_gols)
+else:
+    ficha(nome_jogador, qtd_gols)
