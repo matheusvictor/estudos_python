7605c00b4218cc595f650ca446c4cf627e30dae8
Matheus Victor <matheusvictor.salles@gmail.com>
2022-02-04 17:16:56 -0300

    Atualiza resolução do ex115 (parte 3)
    Nessa implementação, o módulo de arquivo é capaz de:
    
    - Contar a quantidade de linhas do arquivo
    - Cadastrar uma pessoa (realizar um registro no arquivo de texto)

--- curso_em_video/mundo_03/ex115/lib/arquivo/__init__.py
+++ curso_em_video/mundo_03/ex115/lib/arquivo/__init__.py
@@ -4,12 +4,13 @@ from curso_em_video.mundo_03.ex115.lib.interface import *
 
 def arquivoExiste(nome_arquivo='repositorio.txt'):
     try:
-        print('Localizando arquivo...')
-        sleep(2)
+        print('Localizando arquivo de dados...')
+        sleep(1)
         arquivo = open(nome_arquivo, 'rt')
         arquivo.close()
     except FileNotFoundError:
-        entrada = input('Arquivo não encontrado!\nDeseja criar o arquivo agora? [S/N] >> ')[0].strip()
+        print(cor_texto('Arquivo não encontrado!', 1))
+        entrada = input('Deseja criar o arquivo agora? [S/N] >> ')[0].strip()
         if entrada in 'Ss':
             print(f'Criando arquivo "{nome_arquivo}"...')
             criarArquivo(nome_arquivo)
@@ -47,4 +48,31 @@ def lerArquivo(nome_arquivo='repositorio.txt'):
             print(cor_texto('Criação de arquivo cancelada pelo usuário!', 1))
     else:
         cabecalho('Pessoas cadastradas')
-        print(arquivo.readlines())
+        if contador_linhas_arquivo() > 0:
+            print(arquivo.read())
+        else:
+            print(cor_texto('Não contém registros no arquivo!', 1))
+
+
+def contador_linhas_arquivo(nome_arquivo='repositorio.txt'):
+    with open(nome_arquivo) as arquivo:
+        qtd_linhas = 0
+        for linha in arquivo:
+            qtd_linhas += 1
+    return qtd_linhas
+
+
+def cadastrar_pessoa(nome_arquivo='repositorio.txt', nome='<desconhecido>', idade=0):
+    try:
+        arquivo = open(nome_arquivo, 'at')
+    except:
+        print(cor_texto('Houve um erro ao tentar abrir o arquivo!', 1))
+    else:
+        try:
+            arquivo.write(f'{nome};{idade}\n')
+        except:
+            print(cor_texto('Houve um erro ao inserir os dados no arquivo!', 1))
+        else:
+            print(cor_texto(f'Registro de {nome} realizado com sucesso!', 2))
+        finally:
+            arquivo.close()
