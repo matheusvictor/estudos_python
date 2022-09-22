6b7d367747cc98ba0203f870074019dc93a41403
Matheus Victor <matheusvictor.salles@gmail.com>
2022-09-22 13:57:27 -0300

    feature: Implementa condição de vitória ou derrota

--- alura/jogo_da_forca/forca.py
+++ alura/jogo_da_forca/forca.py
@@ -1,30 +1,44 @@
+import data
+
+
 def jogar():
     print('*' * 40)
     print('Boas-vindas ao jogo da forca!'.upper())
     print('*' * 40)
 
-    perdeu = False
     acertou = False
-    palavra_secreta = 'PYTHON'
-
+    enforcou = False
+    numero_erros = 0
+    palavra_secreta = 'PYTHON'.strip().upper()
     palavra_secreta_mascarada = ['_'] * len(palavra_secreta)
+
     print(f'\n{palavra_secreta_mascarada}')
     print(f'Dica: a palavra possui {contar_letras_palavra_secreta(palavra_secreta)} letra(s)!\n')
 
-    while not acertou and not perdeu:
+    while not acertou and not enforcou:
 
         palpite = input('Qual seu palpite?: ').strip().upper()
 
-        index = 0
         letras_acertadas = list()
 
-        for letra in palavra_secreta:
-            if letra == palpite:
-                letras_acertadas.append(letra)
-                palavra_secreta_mascarada[index] = letra
-                print(palavra_secreta_mascarada)
-            index += 1
-    print('Fim de jogo')
+        if palpite in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if letra == palpite:
+                    letras_acertadas.append(letra)
+                    palavra_secreta_mascarada[index] = palpite
+                index += 1
+        else:
+            numero_erros += 1
+
+        acertou = '_' not in palavra_secreta_mascarada
+        enforcou = numero_erros == len(palavra_secreta)
+
+    if acertou:
+        print('Você ganhou'.upper())
+    else:
+        print('Você perdeu'.upper())
+    print('Fim de jogo'.upper())
 
 
 def gerar_palavra_secreta():
