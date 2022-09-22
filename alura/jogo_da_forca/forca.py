2557b4e20849ed7a84ab53524666b60845b024ea
Matheus Victor <matheusvictor.salles@gmail.com>
2022-09-22 13:14:27 -0300

    feature: Implementa lista para guardar letras acertadas pelo usuário

--- alura/jogo_da_forca/forca.py
+++ alura/jogo_da_forca/forca.py
@@ -1,16 +1,45 @@
 def jogar():
-    print()
+    print('*' * 40)
+    print('Boas-vindas ao jogo da forca!'.upper())
+    print('*' * 40)
 
     perdeu = False
     acertou = False
     palavra_secreta = 'PYTHON'
 
+    palavra_secreta_mascarada = ['_'] * len(palavra_secreta)
+    print(f'\n{palavra_secreta_mascarada}')
+    print(f'Dica: a palavra possui {contar_letras_palavra_secreta(palavra_secreta)} letra(s)!\n')
+
     while not acertou and not perdeu:
 
         palpite = input('Qual seu palpite?: ').strip().upper()
 
+        index = 0
+        letras_acertadas = list()
+
         for letra in palavra_secreta:
             if letra == palpite:
-                print(letra)
+                letras_acertadas.append(letra)
+                palavra_secreta_mascarada[index] = letra
+                print(palavra_secreta_mascarada)
+            index += 1
+    print('Fim de jogo')
 
-        print('Fim de jogo')
+
+def gerar_palavra_secreta():
+    # TODO
+    pass
+
+
+def transformar_palavra_em_minusculas():
+    # TODO
+    pass
+
+
+def transformar_palavra_em_maiusculas():
+    pass
+
+
+def contar_letras_palavra_secreta(palavra):
+    return len(palavra)
