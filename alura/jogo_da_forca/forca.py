e3c82e1e67042a5bf966ecd5706ca0d518527f1b
Matheus Victor <matheusvictor.salles@gmail.com>
2022-09-23 10:48:14 -0300

    update: Cria funções para isolar rotinas

--- alura/jogo_da_forca/forca.py
+++ alura/jogo_da_forca/forca.py
@@ -4,17 +4,13 @@ import unidecode
 
 
 def jogar():
-    print('*' * 80)
-    print('Boas-vindas ao jogo da forca!'.upper())
-    print('*' * 80)
+    imprimir_boas_vindas()
+    palavra_secreta = gerar_palavra_secreta()
+    palavra_secreta_mascarada = mascarar_palavra_secreta(palavra_secreta)
 
     acertou = False
     enforcou = False
     numero_erros = 0
-    quantidade_dicas_disponiveis = 3  # TODO: implementar funcionalidade de dicas
-
-    palavra_secreta = gerar_palavra_secreta()
-    palavra_secreta_mascarada = ['_' for letra in palavra_secreta]  # compreensão de lista
 
     palpites_errados = list()
     palpites_corretos = list()
@@ -23,7 +19,7 @@ def jogar():
 
         print(f'\n{palavra_secreta_mascarada}')
 
-        palpite = input('Qual seu palpite?: ').strip().upper()
+        palpite = dar_palpite()
 
         if palpite in palavra_secreta:
             if palpite in palpites_corretos:
@@ -45,24 +41,25 @@ def jogar():
                 palpites_errados.append(palpite)
                 print(f'Você ainda possui {len(palavra_secreta) - numero_erros} chances!')
 
-        acertou = '_' not in palavra_secreta_mascarada
-        enforcou = numero_erros == len(palavra_secreta)
+        acertou = verificar_vitoria(palavra_secreta_mascarada)
+        enforcou = verificar_derrota(numero_erros, palavra_secreta)
 
     if acertou:
-        print('*' * 80)
-        print('Você ganhou!'.upper())
-        print(f'Para acertar a palavra secreta, você utilizou {quantidade_palpites_dados} '
-              f'palpites (desconsiderando palpites repetidos)')
+        imprimir_mensagem_vitoria()
     else:
-        print('*' * 80)
-        print('Você perdeu!'.upper())
+        imprimir_mensagem_derrota(palavra_secreta)
 
+    # TODO : corrigir contagem de palpites
+    print(f'Você utilizou {quantidade_palpites_dados} palpites (desconsiderando palpites repetidos).')
+
+
+def imprimir_boas_vindas():
     print('*' * 80)
-    print('Fim de jogo!'.upper())
+    print('Boas-vindas ao jogo da forca!'.upper())
     print('*' * 80)
 
 
-def gerar_palavra_secreta():
+def gerar_palavra_secreta() -> str:
     arquivo = open(file='./data/banco_de_palavras.txt', mode='r', encoding='utf-8')
     palavras = []
 
@@ -74,6 +71,58 @@ def gerar_palavra_secreta():
     indice_randomico_palavra = randrange(start=0, stop=len(palavras))
 
     return unidecode.unidecode(palavras[indice_randomico_palavra])
+
+
+def mascarar_palavra_secreta(palavra_secreta):
+    return ['_' for letra in palavra_secreta]  # compreensão de lista
+
+
+def dar_palpite():
+    palpite = input('Qual seu palpite?: ').strip().upper()
+    return palpite
+
+
+def verificar_vitoria(palavra_secreta_mascarada):
+    return '_' not in palavra_secreta_mascarada
+
+
+def verificar_derrota(numero_erros, palavra_secreta):
+    return numero_erros == len(palavra_secreta)
+
+
+def imprimir_mensagem_vitoria():
+    print("Parabéns, você ganhou!".upper())
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+
+def imprimir_mensagem_derrota(palavra_secreta):
+    print("Você perdeu!".upper())
+    print("A palavra era {}".format(palavra_secreta).upper())
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
 
 
 def cadastrar_nova_palavra():
