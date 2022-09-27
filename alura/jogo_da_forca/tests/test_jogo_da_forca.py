5c6512eb03f94324721a9c48c0b2d8b76e765a22
Matheus Victor <matheusvictor.salles@gmail.com>
2022-09-27 10:29:38 -0300

    feat/test: Adiciona teste para verificar se o palpite completo é a palavra secreta

--- alura/jogo_da_forca/tests/test_jogo_da_forca.py
+++ alura/jogo_da_forca/tests/test_jogo_da_forca.py
@@ -1,4 +1,4 @@
-from alura.jogo_da_forca.forca import mascarar_palavra_secreta
+from alura.jogo_da_forca.forca import *
 
 
 class TestJogoForca:
@@ -12,4 +12,13 @@ class TestJogoForca:
         resultado = mascarar_palavra_secreta(entrada)
 
         # then
+        assert resultado == esperado
+
+    def test_quando_palpite_completo_for_igual_palavra_secreta_deve_ganhar(self):
+        entrada = 'manga'.upper()
+        palavra_secreta = 'manga'.upper()
+        esperado = True
+
+        resultado = verificar_palavra_completa(entrada, palavra_secreta)
+
         assert resultado == esperado
