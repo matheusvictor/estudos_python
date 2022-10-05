b5d8b622a5a2bd6a487c6a222b7bbe55c17d6613
Matheus Victor <matheusvictor.salles@gmail.com>
2022-10-05 15:01:48 -0300

    Testa comportamentos de herança

--- alura/poo/main.py
+++ alura/poo/main.py
@@ -1,15 +1,25 @@
-from alura.poo.modelo.filme import Filme
 from alura.poo.modelo.serie import Serie
+from alura.poo.modelo.filme import Filme
 from alura.poo.modelo.playlist import Playlist
 
-if __name__ == '__main__':
-    filme = Filme('Transformers', 2018, 60)
-    serie = Serie('narcos', 2018, 2)
-    filme.nome
-    print(filme.likes)
-    serie.dar_likes()
-    print(serie.likes)
-    print(serie)
+# from alura.poo.modelo.programa_tv import ProgramaTV
 
-    play = Playlist('Minha playlist', [serie])
-    print(play.obter_tamanho_playlist([serie]))
+if __name__ == '__main__':
+    narcos = Serie('Narcos', 2015, 3)
+    transformers = Filme('Transformers', 2007, 144)
+
+    minha_playlist = Playlist('Minha playlist', [narcos, transformers])
+
+    for programa in minha_playlist:
+        print(programa)
+
+    narcos.dar_likes()
+    transformers.dar_likes()
+    transformers.dar_likes()
+
+    print(f'Tamanho da playlist: {minha_playlist.tamanho}')
+
+    for programa in minha_playlist:
+        print(programa)
+
+    # programa = ProgramaTV('Telecurso', 2000) # impossível instanciar porque é uma classe abstrata
--- alura/poo/modelo/serie.py
+++ alura/poo/modelo/serie.py
@@ -5,3 +5,7 @@ class Serie(ProgramaTV):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.__temporadas = temporadas
+
+    def __str__(self):
+        return f'Série "{self.nome}", lançada em {self.ano} e possui {self.__temporadas} temporada(s). ' \
+               f'Possui {self.likes} like(s).'
